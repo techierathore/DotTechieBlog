2762cdf50cc2c4ef404b6ef1098b9be9eb0994bf
S Ravi Kumar <techierathore@gmail.com>
2022-06-26 15:27:07 +0530

    Updated Packages

--- BlogEngine/DataAccess/BaseDa.cs
+++ BlogEngine/DataAccess/BaseDa.cs
@@ -1,22 +1,19 @@
-﻿using Dapper;
-using MySql.Data.MySqlClient;
-using System;
+﻿using MySql.Data.MySqlClient;
 using System.Configuration;
 using System.Data;
-using System.Linq;
 
 namespace BlogEngine.DataAccess
 {
-    public abstract class BaseDa
+    public class BaseDa
     {
-        protected static void SetIdentity<T>(IDbConnection connection, Action<T> setId)
-        {
-            dynamic identity = connection.Query("SELECT @@IDENTITY AS Id").Single();
-            var newId = (T)identity.Id;
-            setId(newId);
-        }
+        //protected static void SetIdentity<T>(IDbConnection connection, Action<T> setId)
+        //{
+        //    dynamic identity = connection.Query("SELECT @@IDENTITY AS Id").Single();
+        //    T newId = (T)identity.Id;
+        //    setId(newId);
+        //}
 
-        protected static IDbConnection OpenConnection()
+        public static IDbConnection OpenConnection()
         {
             var vConString = ConfigurationManager.ConnectionStrings["BlogDbUrl"].ConnectionString;
             //var vDecryptedConString = AstroEncrypt.AstroLyfeDecrypt(vConString);
